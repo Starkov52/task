66d46973b43f66b45a64e404030fdd101663bf0e
Starkov52 <valuznnicartem@gmail.com>
2025-08-11 19:44:11 +0300

    fix before deploy for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import FormSections from './components/formSections'
 import ProductTable from './components/productTabel'
 import React from 'react'
